0c994ea1a729c90a2595132f4ec1f938e2b538bf
Alexander Inozemtsev <ai.inozemtsev@jet.su>
2020-08-11 19:34:30 +0300

    import connect

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -4,6 +4,7 @@ import {
   updateNewMessageActionCreator,
 } from "../../redux/dialogs-reducer";
 import { Dialogs } from "./Dialogs";
+import { connect } from "react-redux";
 import { StoreContext } from "../../StoreContext";
 
 export const DialogsContainer = ({ store }) => {
